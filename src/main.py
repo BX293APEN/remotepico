5e5aa78df0566a3b3292b18f29845cae9afa5b34
PEN <uemasaki20011222@gmail.com>
2026-05-16 17:29:28 +0900

    Raspberry Pi Pico

--- src/main.py
+++ src/main.py
@@ -3,21 +3,23 @@ from machine import (
     UART
 )
 
-def handler(pin):
-    print("IRQ from:", pin.pinID)
+
 
 class Button(Pin):
-    def __init__(self, pin, mode):
-        self.pinID = pin
-        super().__init__(self.pinID, Pin.IN, mode)
+    def __init__(self, pinID, mode, setInterrupt = True):
+        self.pinID = pinID
+        self.pin = super().__init__(self.pinID, Pin.IN, mode)
+        if setInterrupt:
+            self.irq(trigger=Pin.IRQ_FALLING, handler=self.handler)
     
+    def handler(self, pin):
+        print("IRQ from:", self.pinID)
 
 class RemotePico:
     def __init__(self): 
         self.picoPinList = []
         for p in [0, 4, 7, 8, 11, 15, 19]:
             btn = Button(p, Pin.PULL_UP)
-            btn.irq(trigger=Pin.IRQ_FALLING, handler=handler)
             self.picoPinList.append(btn)
     
     def run(self):
